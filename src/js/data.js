0317e30d98fbedf1fb724d321e6c8ac8a403a75f
Drewf1 <andyfadin96@gmail.com>
2022-05-23 18:23:02 +0300

    first commit

---
+++ src/js/data.js
@@ -0,0 +1,50 @@
+const data = {
+  first_name: "Andrii",
+  last_name: "Fadin",
+  job_title: "FULL STACK DEVELOPER",
+  photo: "images/IMG_3950.JPG",
+  dateOfBirth: "Date of birth: 17.12.1996",
+  phone: "+38 (097) 283-32-69",
+  email: "andyfadin96@gmail.com",
+  address: "Lesya Kurbasa Ave, 18, Kyiv, 03162, Ukraine",
+  skills: [
+    "HTML & CSS",
+    "Project Management",
+    "Contact Negotiation",
+    "Product Positioning",
+    "Product Presentation",
+    "Adobe Creative Suite",
+    "Microsoft Office 2016",
+    "AutoCAD 3D",
+    "Blender 3D",
+    "Fluent in English",
+    "Beginner in German",
+  ],
+  education: [
+    [
+      "2018 - 2020",
+      "National Aviation University Master degree (Airport Technologies and Technical Equipment).",
+    ],
+    [
+      "2014 - 2018",
+      "National Aviation University Bachelor Degree (Airport Technologies and transport infrastructure)",
+    ],
+  ],
+  additionalEducation: [
+    ["HTML & CSS for JavaScript Developers by ITGid."],
+    ["JavaScript v.2.0 by ITGid."],
+    ["T.D. Williamson Data Analysis Introduction Training."],
+    ["T.D. Williamson Line Master T-101 by Global HT&P Training."],
+    [
+      "Clock spring|NRI Composite repair installation and quality control of composite repair.",
+    ],
+  ],
+  achievements: [
+    "Utility Patent №141928 27.04.2020 - Pipeline geometry tool",
+    "Certificate “T.D. Williamson Data Analysis Introduction Training on September 9,2020”",
+    "Certificate of completion “T.D. Williamson Line Master T-101 by Global HT&P Training”",
+    "Certificate “Clock spring|NRI Composite repair installation and quality control of composite repair”",
+  ],
+  profile:
+    "A versatile, analytical and hard-working IT-beginner with a practical 'get the thing done' approach, who makes everything to achieve the best results. Former engineer passioned about Web Development world.",
+};
---
+++ src/js/experience.js
@@ -0,0 +1,18 @@
+const experience = [
+    {
+        "company": "Active Alliance LLC",
+        "city": "Kyiv",
+        "from": "10.2018",
+        "to": '04.2022',
+        "position": "Chief Engineer",
+        "description": "Pipeline cleaning and diagnostics (ILI), preliminary and final reports preparation in accordance with actual Ukrainian standards (DSTU), Ukrainian gas & oil industry standards (SOU) and international (POF 2009/2016) standards, intellectual pigs (MFL, Geometry tool) raw data analysis and interpretation, pipetally preparation, cleaning pigs modifications design, cleaning pigs drawings, cleaning pigs spare parts drawings, cleaning pigs 3D drawings, pipeline cleaning pigs manufacturing and assembly, standards and equipment documentation (manuals, instructions) translation (English – Russian/Ukrainian, Russian/Ukrainian – English), company solutions and services presentations."
+    },
+    {
+        "company": "Premier Aquatics USA",
+        "city": "Alexandria, VA",
+        "from": "05.2016",
+        "to": '10.2016',
+        "position": "Lifeguard",
+        "description": "Customer counseling, pool monitoring, swimming training, maintenance of pool and water at proper conditions, pumps and pipes maintenance, first aid."
+    },
+]
